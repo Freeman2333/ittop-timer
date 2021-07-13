79789fe43b8f39ee2354d92cf6ea652dba92ea35
s.popov <s.popov@indevlab.com>
2021-07-13 15:13:10 +0300

    пауза

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import TimerDisplay from './TimerDisplay'
-
+import style from './Timer.module.css'
 const Timer = () => {
+
+    const [ elapsed, setElapsed ] = useState({ seconds: 0 })
+	const [ running, setRunning ] = useState(false)
+	const [ waiting, setWaiting ] = useState(false)
+
+    const toggleStartStop = ()=>{
+        if (running && waiting) {
+            setWaiting(false)
+			setRunning(true)
+        } else setRunning(!running)
+    }
+    useEffect(()=>{
+        if(running&& waiting)return
+    })
     return (
-        <div>
-            <TimerDisplay/>
+        <div className={style.container}>
+            <TimerDisplay seconds={elapsed.seconds}/>
+            <div className={style.btnContainer}>
+                <button>
+                    start
+                </button>
+                <button>
+                    resume
+                </button>
+                <button>
+                    reset
+                </button>
+            </div>
         </div>
     )
 }
